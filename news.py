945e104b6492c02935a8077514c2f22dc5bfac98
Pramurta <pramurta.chatterjee@gmail.com>
2020-07-01 05:29:29 +0800

    Edited news.py file

--- news.py
+++ news.py
@@ -1,27 +1,26 @@
-import pandas as pd
-import requests,json
-import datetime 
+import pycountry, datetime, requests,json
+country=input("Enter a country's name: ")
+countryClass=pycountry.countries.get(name=country)
+code=countryClass.alpha_2
+Previous_Date = datetime.datetime.today() - datetime.timedelta(days=15) 
+dt=str(Previous_Date)[:10]
+ 
+url_practice="https://newsapi.org/v2/top-headlines?q=COVID&from="+dt+"&sortBy=popularity&apiKey=565324e8912a47cdbce2d37b8639d5ed&pageSize=100&page=1&country="+code
 
-def return_news(country):
-    page = pd.read_html('https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes')
-    countries=dict()
-    df=page[0]
-    for i in range(len(df)):
-        countries[str(df.iloc[:,0][i])]=str(df.iloc[:,3][i])
-   
-    Previous_Date = datetime.datetime.today() - datetime.timedelta(days=15) 
-    dt=str(Previous_Date)[:10]
-    url_practice="https://newsapi.org/v2/top-headlines?q=COVID&from="+dt+"&sortBy=popularity&apiKey=565324e8912a47cdbce2d37b8639d5ed&pageSize=100&page=1&country="+countries[country]
+payload = {}
+headers= {}
 
-    payload = {}
-    headers= {}
+response = requests.request("GET", url_practice, headers=headers, data = payload)
+json_data = response.json()
 
-    response = requests.request("GET", url_practice, headers=headers, data = payload)
-    json_data = response.json()
-    answer=list()
-    for i in range(len(json_data['articles'])):
-        answer.append([json_data['articles'][i]['source']['name'],json_data['articles'][i]['title'],json_data['articles'][i]['url']])
-    return answer
+answer=list()
+for i in range(len(json_data['articles'])):
+    answer.append([json_data['articles'][i]['source']['name'],json_data['articles'][i]['title'],json_data['articles'][i]['url']])
 
-news=return_news('India')  #Country name goes here
-print(news)
+print(answer)
+
+
+
+
+
+
